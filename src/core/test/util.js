63cdc16a6925381abcbade1591ad16c2064c4570
satoooon8888 <samidare256@gmail.com>
2023-12-20 00:29:32 +0900

    remove async keyword

--- src/core/test/util.js
+++ src/core/test/util.js
@@ -2,7 +2,7 @@ import { stub } from "https://deno.land/std@0.209.0/testing/mock.ts";
 
 const stubClaims = (claimMap) => {
   const callback = async (testAsyncFn) => {
-    const fetchStub = stub(globalThis, "fetch", async (url) => {
+    const fetchStub = stub(globalThis, "fetch", (url) => {
       return new Response(JSON.stringify(claimMap[url]));
     });
     try {
