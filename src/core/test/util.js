039d6ba8dcbc5835c1b9d3d1f60f80be288c43cd
satoooon8888 <samidare256@gmail.com>
2023-12-20 22:07:29 +0900

    use promise in stubClaims

--- src/core/test/util.js
+++ src/core/test/util.js
@@ -3,7 +3,7 @@ import { stub } from "https://deno.land/std@0.209.0/testing/mock.ts";
 const stubClaims = (claimMap) => {
   const callback = async (testAsyncFn) => {
     const fetchStub = stub(globalThis, "fetch", (url) => {
-      return new Response(JSON.stringify(claimMap[url]));
+      return Promise.resolve(new Response(JSON.stringify(claimMap[url])));
     });
     try {
       await testAsyncFn();
