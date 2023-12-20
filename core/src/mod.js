051aa3c4cc5098a79b2c7d85e313d43a18e069a0
satoooon8888 <samidare256@gmail.com>
2023-12-20 23:49:34 +0900

    refactor directory structure

--- core/src/mod.js
+++ core/src/mod.js
@@ -1,6 +1,5 @@
 import { validateClaim } from "./validate.js";
 import { fetchClaim } from "./fetch.js";
-import { walkWholeClaimMap } from "./walk.js";
-import { claimSchema } from "./walk.js";
+import { claimSchema, walkWholeClaimMap } from "./walk.js";
 
 export { claimSchema, fetchClaim, validateClaim, walkWholeClaimMap };
--- core/src/validate.js
+++ core/src/validate.js
@@ -1,7 +1,7 @@
 import Ajv from "https://esm.sh/ajv@8.12.0";
 import addFormats from "https://esm.sh/ajv-formats@2.1.1";
 
-const { default: claimSchema } = await import("../schema/claim.schema.json", {
+const { default: claimSchema } = await import("./schema/claim.schema.json", {
   assert: { type: "json" },
 });
 
