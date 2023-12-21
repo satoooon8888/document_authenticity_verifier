f3fe0373c5e6c72a42625060096db5a499e5fa98
satoooon8888 <samidare256@gmail.com>
2023-12-21 11:44:22 +0900

    improve verify view

--- cli/src/verify.js
+++ cli/src/verify.js
@@ -2,9 +2,18 @@ import { walkWholeClaimMap } from "../../core/src/mod.js";
 
 const verify = async (url) => {
   const { claimMap, authenticated, errors } = await walkWholeClaimMap(url);
-  if (authenticated) {
-    console.log(`${url} is authenticated`);
+  console.log(claimMap);
+  errors.map((error) => {
+    console.log(`[Error] ${error}`);
+  });
+  Object.entries(authenticated)
+    .filter(([id, v]) => v === false)
+    .map(([id, v]) => {
+      console.log(`[Warning] ${id} is not authenticated!`);
+    });
+  if (authenticated[url]) {
+    console.log(`[*] ${url} is authenticated.`);
   } else {
-    console.log(`${url} has unauthenticated claim!`);
+    console.log(`[*] ${url} has unauthenticated claim!`);
   }
 };
