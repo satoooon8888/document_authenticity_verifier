a6fd848b1f3b283b69f3f2d5cbbc7f917a2a3586
satoooon8888 <samidare256@gmail.com>
2024-01-12 11:12:56 +0900

    add build for core package

---
+++ core/build.js
@@ -0,0 +1,20 @@
+// ex. scripts/build_npm.ts
+import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./src/mod.js"],
+  outDir: "./npm",
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  package: {
+    // package.json properties
+    name: "@document-authenticity-verifier/core",
+    version: Deno.args[0],
+    description: "hogehoge",
+  },
+  importMap: "deno.json",
+});
